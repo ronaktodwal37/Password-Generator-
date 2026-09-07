6a66bff538ae428b291ed576dd2a2e892b214358
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-07 19:07:24 +0530

    done changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ const passwordRef = useRef(null)
     // if(numberAllowed) str+= "0123456789"
     // if(charAllowed) str+= "!@#$%^&*()-_+={}[]~"
 
-    for(let i=1;i<=length;i++){
+    // for(let i=1;i<=length;i++){
       // let char = Math.floor(Math.random() * str.length)
       // pass += str.charAt(char)
-    }
+    // }
 
     setPassword(pass)
 
