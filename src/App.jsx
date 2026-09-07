3f471a1d6dc07ee115aacccbe3903201e85fcb70
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-07 19:03:31 +0530

    char state added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState ,useCallback , useEffect , useRef} from 'react'
 function App() {
   // const [length, setLength] = useState(8);
   // const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false)
+  // const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
   // useRef hook 
