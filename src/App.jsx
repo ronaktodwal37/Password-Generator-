f496a4e0a373e66ea823828d3a02252ef3d80116
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-07 19:04:14 +0530

    str removed

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const passwordRef = useRef(null)
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if(numberAllowed) str+= "0123456789"
+    // if(numberAllowed) str+= "0123456789"
     if(charAllowed) str+= "!@#$%^&*()-_+={}[]~"
 
     for(let i=1;i<=length;i++){
