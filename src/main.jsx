88b42631cac145a3a421c8346a8fc720c54ed282
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-21 15:39:37 +0530

    main js

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@
 // import App from './App.jsx'
 
 // createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  {/* </StrictMode>, */}
 // )
 
