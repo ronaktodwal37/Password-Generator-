f10e931688eaf88987fd24bb7b4153a10504988d
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-21 15:36:11 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
-    // let pass = ""
+    let pass = ""
     // let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     // if(numberAllowed) str+= "0123456789"
